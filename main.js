2e27b57843691b096d6718f1297c73b8060023ab
A-serna0415 <69919518+A-serna0415@users.noreply.github.com>
2021-09-04 21:15:21 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,234 @@
+window.sr = ScrollReveal ();
+
+    sr.reveal('.header-texto', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+    sr.reveal('.content-descripcion', {
+        duration: 1800,
+        origin: 'right',
+        distance: '-50px'
+    });
+
+    sr.reveal('.section1', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+    sr.reveal('.content-section2', {
+        duration: 1800,
+        origin: 'right',
+        distance: '-50px'
+    });
+
+    sr.reveal('.content-img-alex', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+    sr.reveal('.img-estuche', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-50px'
+    });
+
+    sr.reveal('.text-section4', {
+        duration: 2000,
+        origin: 'left',
+        distance: '-50px'
+    });
+
+    // ANIM. SECTION MODELO 3D
+
+    sr.reveal('.texto-modelo3d', {
+        duration: 1800,
+        origin: 'right',
+        distance: '-50px'
+    });
+
+    sr.reveal('.section-modelo3d', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+
+
+    sr.reveal('.img-objeto-interno', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-50px'
+    });
+
+    sr.reveal('.img-objeto-externo', {
+        duration: 2000,
+        origin: 'left',
+        distance: '-50px'
+    });
+
+
+    // ANIM. TEXTO INTERFAZ TACTIL
+
+    sr.reveal('.texto-cuerpo-ciborg', {
+        duration: 1800,
+        origin: 'right',
+        distance: '-50px'
+    });
+
+    sr.reveal('.img-interfaz-tactil-1', {
+        duration: 1800,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+    sr.reveal('.img-interfaz-tactil-2', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-70px'
+    });
+
+    sr.reveal('.img-interfaz-tactil-3', {
+        duration: 2200,
+        origin: 'bottom',
+        distance: '-90px'
+    });
+
+    sr.reveal('.texto-interfaz-gustos', {
+        duration: 1800,
+        origin: 'right',
+        distance: '-50px'
+    });
+
+
+    // ANIM. IMG INTERFAZ TACTIL
+
+    sr.reveal('.interfaz1', {
+        duration: 1800,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+    sr.reveal('.interfaz2', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+    sr.reveal('.interfaz3', {
+        duration: 2200,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+    sr.reveal('.interfaz4', {
+        duration: 2400,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+    sr.reveal('.interfaz5', {
+        duration: 2600,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+
+    // ANIM. TEXTO ASISTENTE VIRTUAL
+
+    sr.reveal('.asistente-virtual', {
+        duration: 1800,
+        origin: 'right',
+        distance: '-50px'
+    });
+
+
+    // ANIM. IMG ASISTENTE VIRTUAL
+
+    sr.reveal('.img-pipe-interfaz', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+
+    // ANIM. TEXTO NUEVO SENTIDO
+
+    sr.reveal('.container-nuevo-sentido', {
+        duration: 1800,
+        origin: 'right',
+        distance: '-50px'
+    });
+
+
+    // ANIM. IMG NUEVO SENTIDO
+
+    sr.reveal('.img-nuevo-sentido', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+
+    // ANIM. TEXTO SONORO-FACIAL
+
+    sr.reveal('.container-texto-sonorofacial', {
+        duration: 1800,
+        origin: 'right',
+        distance: '-50px'
+    });
+
+    // ANIM. IMG SONORO-FACIAL 
+
+    sr.reveal('.img-expresiones-sonorofaciales', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+
+    // ANIM. TEXTO TU CARA ME SUENA
+
+    sr.reveal('.container-texto-suena', {
+        duration: 1800,
+        origin: 'right',
+        distance: '-50px'
+    });
+
+
+    // ANIM. ICONOS EMOCIONES
+
+    sr.reveal('.section17', {
+        duration: 1800,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+    sr.reveal('.section18', {
+        duration: 1800,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+
+    // ANIM. TEXTO REPRODUCIR MUSICA
+
+    sr.reveal('.container-texto-musica', {
+        duration: 1800,
+        origin: 'right',
+        distance: '-50px'
+    });
+
+    // ANIM. IMG REPRODUCIR MUSICA
+
+    sr.reveal('.img-reproducir-musica', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+
+
